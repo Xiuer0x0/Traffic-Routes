d9a9402bc51626864aa86dc82171bc6790cf03f6
Xiuer0x0 <float800508@hotmail.com>
2021-03-09 17:14:55 +0800

    init

---
+++ webpack.dev.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { merge: webpackMerge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+
+const service = {
+    host: 'localhost',
+    port: 58080,
+    get url() {
+        const port = (this.port) ? `:${this.port}` : '';
+
+        return `http://${this.host}${port}`;
+    },
+};
+
+module.exports = webpackMerge(commonConfig, {
+    mode: 'development',
+    watch: true,
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        inline: true,
+        host: service.host,
+        port: service.port,
+        hot: true,
+        open: {
+            app: ['chrome', '--incognito', '--other-flag'],
+        },
+        // openPage: [service.url, './test.html'],
+        // historyApiFallback: {
+        //     rewrites: [
+        //         { from: /./, to: service.url },
+        //     ],
+        // },
+    },
+    plugins: [
+    ],
+});
