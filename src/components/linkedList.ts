0b152b59356c59178e43d8f5ad03f9e8dfa9a2ad
Xiuer0x0 <float800508@hotmail.com>
2021-03-11 17:29:40 +0800

    [Feat] class GenericLinkedList<T>.length .at
    at: 尋找 index 的值
    先判定 index 靠近頭尾的哪側，才遞巡

--- src/components/linkedList.ts
+++ src/components/linkedList.ts
@@ -28,3 +28,75 @@ class GenericLinkedListNode<T> implements LinkedListNode<T> {
     constructor(public value: T) {
     }
 }
+
+class GenericLinkedList<T> implements LinkedList<T> {
+    public head: LinkedListNode<T> | null = null;
+    public foot: LinkedListNode<T> | null = null;
+    
+    private count = 0;
+
+    public get length() {
+        return this.count;
+    }
+
+    private get medianIndex() {
+        return this.length / 2;
+    }
+
+    private checkIndex(index: number): boolean {
+        if (index < 0) {
+            throw new Error(`Index must be positive: ${index}`);
+        }
+
+        if (index >= this.length) {
+            console.warn(`index ${index} out of bounds.`);
+            return false;
+        }
+
+        return true;
+    }
+    
+    public at(index: number) {
+        if (!this.checkIndex(index)) {
+            return null;
+        }
+
+        let isForward = (index < this.medianIndex);
+        let currentNode = isForward ? this.head : this.foot;
+        let currentIndex = isForward ? 0 : (this.length - 1);
+
+        while (currentIndex !== index) {
+            currentNode = (isForward
+                ? currentNode?.next
+                : currentNode?.prev
+            ) as LinkedListNode<T>;
+            currentIndex += isForward ? 1 : -1;
+        }
+        
+        return currentNode;
+    }
+
+    public insert(index: number, value: T) {
+        
+    }
+
+    public unshift(value: T) {
+        
+    }
+
+    public push(value: T) {
+
+    }
+
+    public remove(index: number) {
+
+    }
+
+    public shift() {
+
+    }
+
+    public pop() {
+
+    }
+}
