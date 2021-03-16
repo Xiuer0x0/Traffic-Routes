b3bde48a94d37e80832d2939a24d976d6a93e6d0
Xiuer0x0 <float800508@hotmail.com>
2021-03-16 11:25:03 +0800

    [Refector] busData.d.ts
    1. 將 Source 纇的 interface 歸納至 Source
    2. 增加 key 說明與標注 Soure 對應來源檔案名稱
    3. Rename:
        + interface Path -> RoadMap
        + interface RouteDirectionInfo -> PathSequence
    4. interface PathFilter extends PathSequence

--- src/components/dataProcess/busData.d.ts
+++ src/components/dataProcess/busData.d.ts
@@ -2,6 +2,90 @@ import { LatLngExpression } from "leaflet";
 import { LinkedList } from "../linkedList";
 
 declare namespace BusData {
+    namespace Source {
+        /** GetStop.json */
+        interface Stop {
+            /** 站牌代碼 */
+            Id: number;
+            /** 所屬路線代碼 (主路線 ID) */
+            routeId: number;
+            /** 地址 */
+            address: string;
+            /** 去返程 （0：去程 / 1：返程 / 2：未知） */
+            goBack: string;
+            /** 緯度 */
+            latitude: string;
+            /** 經度 */
+            longitude: string;
+            /** 英文名稱 */
+            nameEn: string;
+            /** 中文名稱 */
+            nameZh: string;
+            /** 上下車站別 （-1：可下車、0：可上下車、1：可上車） */
+            pgp: string;
+            /** 於路線上的順序 */
+            seqNo: number;
+            /** 顯示用緯度 */
+            showLat: string;
+            /** 顯示用經度 */
+            showLon: string;
+            /** 站位 ID */
+            stopLocationId: number;
+            /** 向量角：0~359，預設為空白 */
+            vector: string;
+        };
+
+        /** RoadMap.csv */
+        interface RoadMap {
+            /** 所屬業管機關 */
+            Authority: string;
+            /** 去返程 [0: 去程; 1: 返程; 2: 迴圈; 255: 未知] */
+            Direction: number;
+            /** 地理識別碼 */
+            Geohash: string;
+            /** 資料代表日期(yyyy-MM-dd) */
+            InfoDate: string;
+            /** 站牌位置縣市之代碼 */
+            LocationCityCode: string;
+            /** 營運業者編號 */
+            OpreatorNo: number;
+            /** 位置緯度(WGS84) */
+            PositionLat: number;
+            /** 位置緯度(WGS84) */
+            PositionLon: number;
+            /** 來源端平台資料更新時間(yyyy-MM-dd HH:mm:ss) */
+            SrcUpdateTime: string;
+            /** 站牌所屬的站位代碼 */
+            StationID: number;
+            /** 站牌所屬的站名碼 */
+            StationNameID: string | null;
+            /** 站牌所屬的站位唯一代碼 */
+            StationUID: string;
+            /** 上下車站別 [0: 可上下車; 1: 可上車; -1: 可下車] */
+            StopBoarding: number;
+            /** 地區既用中之站牌代碼 */
+            StopID: number;
+            /** 站牌中文名稱 */
+            StopNameEn: string;
+            /** 站牌中文名稱 */
+            StopNameZh: string;
+            /** 路線經過站牌之順序 */
+            StopSequence: number;
+            /** 站牌唯一識別代碼 */
+            StopUID: string;
+            /** 附屬路線唯一識別代碼 */
+            SubrouteUID: string;
+            /** 市區鄉鎮代碼 */
+            TownID: number;
+            /** 市區鄉鎮名稱 */
+            TownName: string;
+            /** 本平台資料更新時間(yyyy-MM-dd HH:mm:ss) */
+            UpdateTime: string;
+            /** 資料版本編號 */
+            VersionID: number;
+        };
+    }
+
     type StopID = number;
 
     interface Stop {
@@ -11,53 +95,13 @@ declare namespace BusData {
         nameZh: string;
     };
 
-    interface StopSource {
-        Id: StopID;
-        routeId: number;
-        address: string;
-        goBack: string;
-        latitude: string;
-        longitude: string;
-        nameEn: string;
-        nameZh: string;
-        pgp: string;
-        seqNo: number;
-        showLat: string;
-        showLon: string;
-        stopLocationId: number;
-        vector: string;
-    };
-
-    interface PathSource {
-        Authority: string;
-        Direction: number;
-        Geohash: string;
-        InfoDate: string;
-        LocationCityCode: string;
-        OpreatorNo: number;
-        PositionLat: number;
-        PositionLon: number;
-        SrcUpdateTime: string;
-        StationID: number;
-        StationNameID: string | null;
-        StationUID: string;
-        StopBoarding: number;
-        StopID: StopID;
-        StopNameEn: string;
-        StopNameZh: string;
-        StopSequence: number;
-        StopUID: string;
-        SubrouteUID: string;
-        TownID: number;
-        TownName: string;
-        UpdateTime: string;
-        VersionID: number;
-    };
-
-    interface PathFilter {
-        direction: number;
+    interface PathSequence {
         stopID: StopID;
         stopSequence: number;
+    };
+
+    interface PathFilter extends PathSequence {
+        direction: number;
     };
 
     interface FilterPathSource {
@@ -65,17 +109,12 @@ declare namespace BusData {
     };
 
     interface RouteDirection {
-        outbound: LinkedList<RouteDirectionInfo> | null;
-        returnTrip: LinkedList<RouteDirectionInfo> | null;
-        cycle: LinkedList<RouteDirectionInfo> | null;
+        outbound: LinkedList<PathSequence> | null;
+        returnTrip: LinkedList<PathSequence> | null;
+        cycle: LinkedList<PathSequence> | null;
     };
 
     interface Routes {
         [routeID: string]: RouteDirection;
-    };
-
-    interface RouteDirectionInfo {
-        stopID: StopID;
-        stopSequence: number;
     };
 };
