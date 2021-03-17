babe8fe9a024dac2e5a6b4de54410a3111862a60
Xiuer0x0 <float800508@hotmail.com>
2021-03-17 16:18:53 +0800

    [Refactor] function fetchBusStop Clean code

--- src/components/dataProcess/busStop.ts
+++ src/components/dataProcess/busStop.ts
@@ -7,20 +7,26 @@ const sourceUrl = '../../assets/data/GetStop.json';
 export default function fetchBusStop(url: string = sourceUrl) {
     return fetchJSON(url)
         .then(response => response.BusInfo as Bus.Source.Stop[])
-        .then(busStopSource => busStopSource.map(info => {
-            const { Id, latitude, longitude, nameEn, nameZh } = info;
-            const busStopInfo: Bus.Stop = {
-                id: Id,
-                name: {
-                    en: nameEn,
-                    zhTW: nameZh,
-                },
-                latLng: <LatLngExpression>[
-                    parseFloat(latitude),
-                    parseFloat(longitude),
-                ],
-            };
-
-            return busStopInfo;
-        }));
+        .then(dataSource => filterSource(dataSource));
 };
+
+function filterSource(source: Bus.Source.Stop[]): Bus.Stop[] {
+    return source.map(data => getBusStopInfo(data));
+}
+
+function getBusStopInfo(source: Bus.Source.Stop): Bus.Stop {
+    const { Id, latitude, longitude, nameEn, nameZh } = source;
+    const busStopInfo: Bus.Stop = {
+        id: Id,
+        name: {
+            en: nameEn,
+            zhTW: nameZh,
+        },
+        latLng: <LatLngExpression>[
+            parseFloat(latitude),
+            parseFloat(longitude),
+        ],
+    };
+
+    return busStopInfo;
+}
