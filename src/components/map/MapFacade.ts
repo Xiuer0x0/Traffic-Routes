dc6e3d000b824d8e8ffaf383c8d56ace0101efc9
Xiuer0x0 <float800508@hotmail.com>
2021-03-19 17:20:17 +0800

    [Feat] MapFacade.focusCoordinates
    聚焦至指定座標

--- src/components/map/MapFacade.ts
+++ src/components/map/MapFacade.ts
@@ -3,11 +3,13 @@ import { CustomMap } from './map';
 import mapConfig, { MapConfig } from './map.config';
 import MapInitializer from './MapInitializer';
 import MapMarkerLayer from './MapMarkerLayer';
+import MapUtility from './MapUtility';
 
 export default class MapFacade {
     private map: L.Map | null = L.map(mapConfig.containerID);
     private mapInitializer: CustomMap.Initializer;
     private mapMarkerLayer: CustomMap.MarkerLayer;
+    private mapUtility: CustomMap.MapUtitity;
 
     constructor(readonly config: MapConfig) {
         if (this.map === null) {
@@ -16,7 +18,12 @@ export default class MapFacade {
 
         this.mapInitializer = new MapInitializer(this.map, config);
         this.mapMarkerLayer = new MapMarkerLayer(this.map);
+        this.mapUtility = new MapUtility(this.map);
 
         this.mapInitializer.initialize();
+    }
+
+    focusCoordinates(lanLng: L.LatLngExpression, options?: CustomMap.ZoomPanOptions) {
+        this.mapUtility.flyTo(lanLng, options);
     }
 }
