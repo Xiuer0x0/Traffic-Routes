f015f9c4c01d233762025e4b19dcf16c6655c1ae
Xiuer0x0 <float800508@hotmail.com>
2021-03-30 17:39:29 +0800

    [Refactor] MapFacade & BusMapFacade
    1. rename
    2. 增加 public 註記
    3. MapFacade 預設參數
    4. BusMapFacade 初始化參數可選填 MapFacade

--- src/components/BusMap.ts
+++ src/components/BusMap.ts
@@ -1,17 +1,52 @@
+import _ from "lodash";
+import L from 'leaflet';
 import { Bus } from "./dataProcess/Bus";
-import mapConfig from './map/map.config';
+import mapConfig, { MapConfig } from './map/map.config';
 import MapFacade from "./map/MapFacade";
 
-export default class BusMapFacade {
-    constructor(
-        private readonly mapFacade: MapFacade = new MapFacade(mapConfig),
-    ) {}
+interface BusMapFacadeOptions {
+    readonly config?: MapConfig;
+    readonly mapFacade?: MapFacade;
+};
 
-    pinStops(data: Bus.Stop[]) {
+export default class BusMapFacade {
+    public readonly config: MapConfig;
+    public readonly mapFacade: MapFacade;
+
+    public constructor({
+        config = mapConfig,
+        mapFacade = new MapFacade(config),
+    }: BusMapFacadeOptions = {}) {
+        this.config = config;
+        this.mapFacade = mapFacade;
+    }
+
+    public drawStops(data: Bus.Stop[]) {
         data.map(obj => {
             const tooltipTemplete = `<div>${obj.name.zhTW}</div>`;
 
             this.mapFacade.drawPin(obj.latLng, tooltipTemplete);    
         });
+    }
+
+    public clearStops() {
+        this.mapFacade.clearPins();
+    }
+
+    public drawPath(data: Bus.Stop[]) {
+        const coordinates = _.reduce(data, (result, values) => {
+            result = [...result, values.latLng];
+
+            return result;
+        }, [] as L.LatLngExpression[]);
+
+        this.clearPath();
+        this.drawStops(data);
+        this.mapFacade.drawPolyline(coordinates);
+    }
+
+    public clearPath() {
+        this.mapFacade.clearPins();
+        this.mapFacade.clearPolyline();
     }
 }
